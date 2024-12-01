a259b678d3a62216e268ffd4347856b591bd8b8a
Iqra Naz <48812001+iqranaz240@users.noreply.github.com>
2024-12-01 16:03:10 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ import { getFirestore, collection, getDocs, addDoc, query, where, getDoc } from 
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwAX7HPPzrEbRiDvS6KkVns8y2IEfVKCY",
-  authDomain: "fir-2b9b9.firebaseapp.com",
-  projectId: "fir-2b9b9",
-  storageBucket: "fir-2b9b9.appspot.com",
-  messagingSenderId: "929690840538",
-  appId: "1:929690840538:web:8163dfad1b5b81e93c24e3",
-  measurementId: "G-FKCSQWWHFX",
+  //add your configs here
 };
 
 // Initialize Firebase
